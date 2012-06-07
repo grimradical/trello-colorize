5dd23b9b7212031011f4423e1f37666934902c34
Deepak Giridharagopal <deepak@puppetlabs.com>
2012-06-07 11:27:27 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 trello-colorize
 ===============
 
-Greasemonkey script for adding background colors to Trello cards
+Greasemonkey script for adding background colors to Trello cards. This is
+helpful for those using Trello for Kanban-esque purposes who want to see, at a
+glance, the breakdown of the types of cards and what's blocked. The background
+color of each card is determined by the color of the label applied to that
+card.
+
+Tested on:
+
+* Chrome, using the Tampermonkey extension
+
+Special cases:
+
+* Purple signifies _blocked_, and is thus rendered as a border around a card of
+  another color as opposed to a background color itself. This is meant to mirror
+  how many teams add a flag or sticker or magnet to cards that are blocked.
+
+Limitations:
+
+* For cards with more than one label, a single color will win.
+
+[Click here](https://github.com/grimradical/trello-colorize/raw/master/trello-colorize.user.js)
+to install.
+
+![Colorized trello cards](https://github.com/grimradical/trello-colorize/raw/master/trello-colorize.png)
---
+++ trello-colorize.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name       Trello card colorizer
+// @namespace  http://github.com/grimradical/trello-colorize
+// @version    0.1
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
+// @description  Changes background color of Trello cards to match labels
+// @match      https://trello.com/board/*
+// @copyright  2012+, Deepak Giridharagopal
+// ==/UserScript==
+
+var colorize = function() {
+    // Blocked
+    $(".purple-label").css("background-color", "#cc99ff");
+    // Slurm
+    $(".green-label").css("background-color", "#99cc66");
+    // Community-related
+    $(".yellow-label").css("background-color", "#ffff99");
+    // ???
+    $(".orange-label").css("background-color", "#ff9966");
+    // Bug
+    $(".red-label").css("background-color", "#ff9999");
+    // Feature
+    $(".blue-label").css("background-color", "#99ccff");
+    // Special visual marker for blocked cards
+    $(".purple-label").filter(".list-card").css("border", "7px solid purple")
+};
+
+setInterval(colorize, 1000);
